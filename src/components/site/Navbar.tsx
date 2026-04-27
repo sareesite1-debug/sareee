f8629c8e87b9f4cfcaaf82b7f5534701826960b7
sareesite1-debug <sareesite1@gmail.com>
2026-04-27 14:24:45 +0530

    Update Navbar.tsx

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -90,7 +90,7 @@ const Navbar = () => {
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.8, duration: 0.5 }}
               >
-                Saree · Center · Mysore
+                Kanchipuram · Pure Silk · Since 1985
               </motion.span>
             </Link>
 
